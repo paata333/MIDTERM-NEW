deec882417260ed769894c3827b5430321c3d16b
paata333 <paata.shvelidze106@gmail.com>
2025-04-24 11:23:13 +0400

    Add files via upload

---
+++ t2/A21.java
@@ -0,0 +1,8 @@
+package t2;
+
+
+public class A21 {
+    public static int width = 5;
+    public static int height = 10;
+}
+
---
+++ t2/A22.java
@@ -0,0 +1,7 @@
+package t2;
+
+
+
+public class A22 {
+    public static int monthNumber = 3;
+}
